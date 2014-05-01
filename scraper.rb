fbb68437f5fa263a6696b16702ac149ce01d05c0
Colin Rood <crrood@gmail.com>
2014-04-30 17:10:26 -0700

    linkedin integration
    log in to LI
    get profile text

--- scraper.rb
+++ scraper.rb
@@ -17,17 +17,26 @@ BEGIN_INDEX = 89484
 END_INDEX = 89493
 
 # get connectifier password at program call
-if ARGV.length != 1
-	puts "please enter connectifier password as argument"
+if ARGV.length != 2
+	puts "please enter connectifier and linkedin passwords as argument"
 	exit
 else
-	password = ARGV[0]
+	cf_password = ARGV[0]
+	li_password = ARGV[1]
 end
 
 # load connectifier extension
 caps = Selenium::WebDriver::Remote::Capabilities.chrome("chromeOptions" => 
 	{ "args" => [ "load-extension=C:/Users/RockIT/AppData/Local/Google/Chrome/User Data/Default/Extensions/mbbpjgnlpelaafnnigciegfpelchjldl/0.6.5_0"]})
 $driver = Selenium::WebDriver.for :chrome, desired_capabilities: caps
+
+# first things first, sign into linkedin
+$driver.navigate.to "http://www.linkedin.com"
+wait_for_element_by_css "input[name='session_key']"
+find_element_by_css("input#session_key-login").send_keys("colin@rockitrecruiting.com")
+find_element_by_css("input#session_password-login").send_keys(li_password)
+find_element_by_css("input#signin").click
+sleep(2)
 
 # for some reason, must reload page to sign in to CF
 $driver.navigate.to "http://stackoverflow.com/users/1"
@@ -43,7 +52,7 @@ find_element_by_css(".fa-sign-in").click
 wait_for_element_by_css "#password"
 
 # submit password
-find_element_by_css("#password").send_keys(password)
+find_element_by_css("#password").send_keys(cf_password)
 find_element_by_css(".fa-sign-in").click
 
 # wait to let the login info sink in
@@ -52,6 +61,8 @@ sleep(3)
 # ...and we're in!
 
 (BEGIN_INDEX..END_INDEX).each do |i|
+
+	candidate_info = {}
 	
 	# conditions:
 	# 1. must be in San Francisco or CA
@@ -69,7 +80,7 @@ sleep(3)
 		# 1. must be in San Francisco or CA
 		location_element = find_element_by_css "td.adr"
 		if location_element.text.match(/San Francisco/i) || location_element.text.match(/CA/) || location_element.text.match(/California/i)
-			puts "valid location"
+			candidate_info["location"] = location_element.text
 		else
 			raise "out of area"
 		end
@@ -83,8 +94,7 @@ sleep(3)
 		
 		# 2. must have linkedin
 		if linkedin_link = find_element_by_css("a[title='LinkedIn profile']")
-			linkedin_url = linkedin_link.attribute("href")
-			puts "linkedin_url: " + linkedin_url
+			candidate_info["linkedin_url"] = linkedin_link.attribute("href")
 		else
 			raise "no linkedin"
 		end
@@ -92,15 +102,13 @@ sleep(3)
 		# 3. must have email or phone number
 		contact_info_present = false
 		if email_link = find_element_by_css("a.email")
-			email_address = email_link.text
-			puts "email: " + email_address
+			candidate_info["email"] = email_link.text
 			contact_info_present = true
 		end
 		
 		if phone_show_btn = find_element_by_css("img.show-button")
 			phone_show_btn.click
-			phone_number = find_element_by_css("a.phone").text
-			puts phone_number
+			candidate_info["phone"] = find_element_by_css("a.phone").text
 			contact_info_present = true
 		end
 		
@@ -108,14 +116,29 @@ sleep(3)
 			raise "no contact info"
 		end
 		
+		# get first and last name
+		name_array = find_element_by_css("span.personName").text.split
+		candidate_info["first_name"] = name_array[0]
+		if name_array.length > 1
+			candidate_info["last_name"] = name_array[1]
+		end
+
+		# load linkedin profile
+		$driver.navigate.to candidate_info["linkedin_url"]
+		wait_for_element_by_css "a.button-secondary"
+
+		# load in recruiter
+		find_element_by_css("a.button-secondary").click
+
+		# wait for the body to load and pull it
+		wait_for_element_by_css "div.content"
+		candidate_info["resume_text"] = find_element_by_css("div.content").text
+
+		# NEXT STEP
+		# put into Compas-recognized .csv format
+
 	rescue Exception => e
 		puts $driver.current_url.match(/[^\/]*$/)[0] + ": " + e.to_s
 	end
-	
-	# download contact info
-
-	# navigate to linkedin profile
-	# load recruiter version
-	# download linkedin profile
 
 end
